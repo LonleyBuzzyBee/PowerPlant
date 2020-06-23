b3cd42253691deca47c54d7ec8c93456e1b1fbd6
mai cook <mailiecook@yahoo.com>
2020-06-23 16:01:46 -0700

    implemented making new plants, and add plant name value

--- src/PowerPlant.js
+++ src/PowerPlant.js
@@ -1,7 +1,7 @@
 
 //this function stores our state
-const storeState = () => {
-  let currentState = {};
+const storeState = (initialState) => {
+  let currentState = initialState;
   return (stateChangeFunction) => {
     const newState = stateChangeFunction(currentState);
     currentState = { ...newState };
@@ -9,53 +9,96 @@ const storeState = () => {
   };
 };
 
-const stateChanger = storeState();
+const stateChanger = storeState({name:"",soil: 0,water:0,light:0});
+const stateChanger2 = storeState({name:"",soil: 0,water:0,light:0});
+
+
+
 
 //this is a function factory. we can easily create more specific functions that alter a plant's soil,water and light to varying degrees.
-  
+
 const changeState = (prop) => {
   return (value) => {
     return (state) => ({
       ...state,
-      [prop]: (state[prop] || 0) + value
+      [prop]: (state[prop] ) + value
     });
   };
 };
+
 const changeStateString = (prop) => {
   return (value) => {
     return (state) => ({
       ...state,
-      [prop]: (state[prop] ||value ) 
+      [prop]: (state[prop] || value ) 
     });
   };
 };
 
 //we creat two functions using our function factory. we could easily create many more.
 
-// const feed = changeState("soil");
+
 const blueFood = changeState("soil")(5);
 const filteredWater = changeState("water")(5);
 const addSun = changeState("light")(5);
-const addPlant = changeStateString("plant")("plant");
+
 //feed(5)(plant)
 $(document).ready(function () {
-  
+  $("form#form1").submit(function(event) {
+    event.preventDefault();
+    let name = $("input#plant").val();
+    const addPlant = changeStateString("name")(name);
+    const newState = stateChanger(addPlant);
+    $('#plant-value').text(newState.name);
+    console.log(newState);
+  });
   //this function has side effects because we are using jquery. Manipulating the DOM will always be a side effect.
   $("#feed").click(function () {
     const newState = stateChanger(blueFood);
     $('#soil-value').text(newState.soil);
+    console.log(newState);
   });
   $("#water").click(function () {
     const newState = stateChanger(filteredWater);
     $('#water-value').text(newState.water);
+    console.log(newState);
   });
   $("#light").click(function () {
     const newState = stateChanger(addSun);
     $('#light-value').text(newState.light);
+    console.log(newState);
   });
-  $("#plant").click(function () {
-    const newState = stateChanger(addPlant);
-    $('#plant-value').text(newState.plant);
+  $("#reset").click(function () {
+    const newPlant = { name: "", soil: 0, water: 0, light: 0 };
+    const newState = newPlant;
+    $('#newP-name').text(newState.name);
+    $('#newP-water').text(newState.water);
+    $('#newP-soil').text(newState.soil);
+    $('#newP-light').text(newState.light);
+    console.log(newState);
+  });
+  $("form#form2").submit(function(event) {
+    event.preventDefault();
+    let name = $("input#newPlant").val();
+    const newPName = changeStateString("name")(name);
+    const newState = stateChanger2(newPName);
+    $('#newP-name').text(newState.name);
+    console.log(newState);
+  });
+  $("#nFeed").click(function () {
+    const newState = stateChanger2(blueFood);
+    $('#newP-soil').text(newState.soil);
+    console.log(newState);
+  });
+  $("#nWater").click(function () {
+    const newState = stateChanger2(filteredWater);
+    $('#newP-water').text(newState.water);
+    console.log(newState);
+  });
+  $("#nLight").click(function () {
+    const newState = stateChanger2(addSun);
+    $('#newP-light').text(newState.light);
+    console.log(newState);
   });
 });
 
@@ -65,3 +108,17 @@ $(document).ready(function () {
 
 
 
+//function sort(inputNumbers) {
+
+// $(document).ready(function() {
+//   $("form").submit(function(event) {
+//     event.preventDefault();
+//     var inputNumbers = $("#count").val();
+//     var result = sort(inputNumbers);
+//    console.log(result);
+//       $(".jumbotron").show();
+//       $("#result").text(result);
+
+    
+//   });
+// });
